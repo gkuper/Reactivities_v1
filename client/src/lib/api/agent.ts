a0e4c5ca3fd0a07203ed5b30da3f4b0b6cd959ab
Greg Kuper <gregkuper@gmail.com>
2025-10-08 15:38:46 -0500

    End of section 9

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,25 +1,33 @@
 import axios from "axios";
+import { store } from "../stores/store";
 
-const sleep =(delay: number) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, delay);
-    })
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  })
 }
 const agent = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: { 'Content-Type': 'application/json' }
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: { 'Content-Type': 'application/json' }
 });
 
 agent.interceptors.response.use(async response => {
-   try {
-     await sleep(1000);
-     return response;
-   } catch (error) {
-     return Promise.reject(error);
-   }
-   
-    
+  try {
+    await sleep(1000);
+
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  } finally {
+    store.uiStore.isIdle();
+  }
+
 });
+
+agent.interceptors.request.use(config => {
+  store.uiStore.isBusy();
+  return config
+})
 
 
 export default agent;
---
+++ client/src/lib/stores/counterStore.ts
@@ -0,0 +1,29 @@
+import { makeAutoObservable } from 'mobx'
+
+export default class CounterStore {
+    title = 'Counter Store';
+    count = 55;
+    events: string[] = [
+    ]
+
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    increment = (amount = 1) => {
+        this.count += amount
+        this.events.push(`Incremented by ${amount} - count is now ${this.count}`)
+    }
+
+    decrement = (amount = 1) => {
+        this.count -= amount
+          this.events.push(`Decremented by ${amount} - count is now ${this.count}`)
+    }
+
+    get eventCount(){
+        return this.events.length
+    }
+
+
+}
